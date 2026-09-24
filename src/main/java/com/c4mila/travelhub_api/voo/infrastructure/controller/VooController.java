c5aa57f83a195e8b9c5b19f7de0365d0d83c25a2
c4amila <camila.ferreira.aut1a@gmail.com>
2026-09-24 16:49:09 -0300

    feat(controller): adiciona endpoint GET para busca de voos por id

--- src/main/java/com/c4mila/travelhub_api/voo/infrastructure/controller/VooController.java
+++ src/main/java/com/c4mila/travelhub_api/voo/infrastructure/controller/VooController.java
@@ -6,10 +6,7 @@ import com.c4mila.travelhub_api.voo.infrastructure.dto.VooRequest;
 import com.c4mila.travelhub_api.voo.infrastructure.dto.VooResponse;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 
@@ -30,5 +27,12 @@ public class VooController {
 
         return ResponseEntity.created(URI.create(PATH_VOOS + "/" + vooResponse.id()))
                 .body(vooResponse);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<VooResponse> buscar(@PathVariable("id") Long id){
+        VooResponse response = vooService.buscarVoo(id);
+
+        return ResponseEntity.ok(response);
     }
 }
