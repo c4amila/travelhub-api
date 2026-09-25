7d6ad593d8423c55484a373773d5621eb99214bc
c4amila <camila.ferreira.aut1a@gmail.com>
2026-09-24 22:08:13 -0300

    feat(repository): adiciona Specification da JPA

--- src/main/java/com/c4mila/travelhub_api/voo/domain/repository/VooRepository.java
+++ src/main/java/com/c4mila/travelhub_api/voo/domain/repository/VooRepository.java
@@ -2,9 +2,10 @@ package com.c4mila.travelhub_api.voo.domain.repository;
 
 import com.c4mila.travelhub_api.voo.domain.model.Voo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface VooRepository extends JpaRepository<Voo, Long> {
+public interface VooRepository extends JpaRepository<Voo, Long>, JpaSpecificationExecutor<Voo> {
     boolean existsByNumeroVoo(String numeroVoo);
 }
